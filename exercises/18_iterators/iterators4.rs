4aa86d800051822dcb444a3e3639a81232f8b5b6
Yttehs <shetty.yttehs@outlook.com>
2025-05-11 19:50:18 +0800

    feat: impl exercise iterators

--- exercises/18_iterators/iterators4.rs
+++ exercises/18_iterators/iterators4.rs
@@ -10,6 +10,16 @@ fn factorial(num: u64) -> u64 {
     // - additional variables
     // For an extra challenge, don't use:
     // - recursion
+
+    // // recursion example
+    // match num {
+    //     0 => 1,
+    //     _ => num * factorial(num - 1)
+    // }
+
+    // (1..=num).fold(1, |acc, next| acc * next)
+
+    (1..=num).product()
 }
 
 fn main() {
